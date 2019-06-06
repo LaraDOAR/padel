bbbd78011d001f452f77aae6c4d6099d9da85a84
lara.dominguez <lara.dominguez@iic.uam.es>
2019-06-07 00:15:13 +0200

    actualizaciones

--- Script/creaPaquete.sh
+++ Script/creaPaquete.sh
@@ -167,12 +167,12 @@ prt_info "Ejecucion..."
 cp Script/index.html "${DIR_TMP}"
 cp ranking.html partidos.html calendario.html "${DIR_TMP}"
 cp -r Librerias "${DIR_TMP}"
-cp doc/Normativa-Federacion.pdf "${DIR_TMP}"
-cp doc/2019-propuestaTorneo.pdf "${DIR_TMP}"
+cp Doc/Normativa-Federacion.pdf "${DIR_TMP}"
+cp Doc/Reglamento-Torneo.pdf "${DIR_TMP}"
 
 # -- se comprime
 cd "${DIR_TMP}"
-zip -r padelToWeb.zip index.html ranking.html partidos.html calendario.html Librerias/* Normativa-Federacion.pdf 2019-propuestaTorneo.pdf
+zip -r padelToWeb.zip index.html ranking.html partidos.html calendario.html Librerias/* Normativa-Federacion.pdf Reglamento-Torneo.pdf
 
 # -- se mueve al directorio actual
 cd - > /dev/null
